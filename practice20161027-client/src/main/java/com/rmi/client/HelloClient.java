57cd44ba29f03c907fa82530bf9c68d638baa408
robert <robert@1204003NB05>
2016-10-28 10:53:23 +0800

    adjust

--- practice20161027-client/src/main/java/com/rmi/client/HelloClient.java
+++ practice20161027-client/src/main/java/com/rmi/client/HelloClient.java
@@ -1,22 +1,41 @@
 package com.rmi.client;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-import com.rmi.server.service.Hello;
+import com.rmi.server.service.Hello; 
 
 public class HelloClient {
 
 	public static void main(String[] args) {
-
+//		new HelloClient().method01(args) ;
+		new HelloClient().method02(args) ;
+	}
+	
+	protected void method01(String[] args){
 		String host = (args.length < 1) ? null : args[0];
+		host ="192.168.7.185" ;
 		try {
-			Registry registry = LocateRegistry.getRegistry("localhost");
-			Hello hello = (Hello) registry.lookup("server.Hello");
+			Registry registry = LocateRegistry.getRegistry(host);
+			Hello hello = (Hello) registry.lookup("hello");
 			System.out.println(hello.sayHello());
 		} catch (Exception e) {
 			System.err.println("Client exception: " + e.toString());
 			e.printStackTrace();
 		}
+	}
+	protected void method02(String[] args){
+		try {
+			Hello rhello = (Hello) Naming.lookup("rmi://192.168.7.185:1099/hello");
+			System.out.println(rhello.sayHello());
+		} catch (Exception e) { 
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		} 
+		
 	}
 }
--- practice20161027-server/src/main/java/com/rmi/server/service/HelloImpl.java
+++ practice20161027-server/src/main/java/com/rmi/server/service/HelloImpl.java
@@ -2,7 +2,10 @@ package com.rmi.server.service;
  
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-
+/**
+ * 
+ * 這邊最關鍵是用了UnicastRemoteObject
+ * */
 public class HelloImpl extends UnicastRemoteObject implements Hello{
 	/**
 	 * 
